29632912e31cf4e70bc8d0fd12858427134db8be
mgrddsj <Jesse_Xu@live.com>
2018-12-18 07:58:56 +0800

    Update ConnectFour.java

--- ConnectFour/src/ConnectFour.java
+++ ConnectFour/src/ConnectFour.java
@@ -14,17 +14,17 @@ public class ConnectFour
 
     public static void printTheBoard(int[][] board)
     {
-		for (int column=0;column<7;column++)
+		for (int row=0;row<6;row++)
 		{
-			for (int row=0;row<6;row++)
+			for (int column=0;column<7;column++)
 			{
 				System.out.print(board[column][row]);
-				if (row != 5)
-				System.out.print("|");
+				if (column != 6)
+					System.out.print("|");
 			}
 			System.out.println();
-			if (column != 6)
-				System.out.println("----------");
+			if (row != 5)
+				System.out.println("-------------");
 		}
     }
 }
