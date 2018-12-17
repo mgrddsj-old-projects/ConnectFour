b3e60d5b73108dc230f29e04de792073a563d1dd
SleepyJesse <Jesse_Xu@live.com>
2018-12-17 12:53:56 +0000

    Did some work with printing the board. Created rules for the array.

--- ConnectFour/src/ConnectFour.java
+++ ConnectFour/src/ConnectFour.java
@@ -1,10 +1,30 @@
 
-public class ConnectFour 
+public class ConnectFour
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-		
+
+
+
+		//Just some testing by Jesse Xu.
+		int[][] board = new int[7][6];
+		printTheBoard(board);
 	}
 
+    public static void printTheBoard(int[][] board)
+    {
+		for (int column=0;column<7;column++)
+		{
+			for (int row=0;row<6;row++)
+			{
+				System.out.print(board[column][row]);
+				if (row != 5)
+				System.out.print("|");
+			}
+			System.out.println();
+			if (column != 6)
+				System.out.println("----------");
+		}
+    }
 }
