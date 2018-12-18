be4e6dd04502db5c384fb49dcdc2840ef15d7b87
AbhishekHemlani <45409109+AbhishekHemlani@users.noreply.github.com>
2018-12-18 08:38:31 +0800

    Update AbhishekMethod.java

--- ConnectFour/src/AbhishekMethod.java
+++ ConnectFour/src/AbhishekMethod.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //Please write your method here. 
 
 public class AbhishekMethod 
@@ -5,7 +7,24 @@ public class AbhishekMethod
 
 	public static void main(String[] args) 
 	{
+	Scanner in = new Scanner(System.in);
+	String[][]board=new String [7][6];
+	System.out.println("choose a game mode:");
+	System.out.println("choose 1 if you want to play player vs player");
+	int a = in.nextInt();
+		if(a==1)
+		{
+			for(int r = 0; r<board.length;r++ )
+			{
+				for(int c = 0; c < board[0].length; c++)
+				{
+					board[r][c]=" ";
+					System.out.print("| " + board[r][c] + " |");
 
+				}
+				System.out.println();
+			}
+		}
 	}
 
 }
